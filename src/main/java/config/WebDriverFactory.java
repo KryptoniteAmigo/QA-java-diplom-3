59bbe29f444f5b7cd5a32b6cfec2e79d447a2dd5
Михаил Карзанов <mixail.teckman@yandex.ru>
2025-03-29 13:01:52 +0300

    Убрал хардкод путей драйверов, переместил в проперти

--- src/main/java/config/WebDriverFactory.java
+++ src/main/java/config/WebDriverFactory.java
@@ -3,16 +3,38 @@ package config;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class WebDriverFactory {
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream input = WebDriverFactory.class
+                .getResourceAsStream("/config.properties")) {
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Не удалось загрузить config.properties", e);
+        }
+    }
 
     public static WebDriver getWebDriver(String browserName) {
         switch (browserName.toLowerCase()) {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", "/WebDriver/bin/chromedriver-win64/chromedriver.exe");
+                String chromeDriverPath = properties.getProperty("chrome.driver.path");
+                if (chromeDriverPath == null || chromeDriverPath.isEmpty()) {
+                    throw new IllegalArgumentException("Не прописан chrome.driver.path в config.properties");
+                }
+                System.setProperty("webdriver.chrome.driver", chromeDriverPath);
                 return new ChromeDriver();
 
             case "yandex":
-                System.setProperty("webdriver.chrome.driver", "/WebDriver/bin/ydriver-win64//yandexdriver.exe");
+                String yandexDriverPath = properties.getProperty("yandex.driver.path");
+                if (yandexDriverPath == null || yandexDriverPath.isEmpty()) {
+                    throw new IllegalArgumentException("Не прописан yandex.driver.path в config.properties");
+                }
+                System.setProperty("webdriver.chrome.driver", yandexDriverPath);
                 return new ChromeDriver();
 
             default:
